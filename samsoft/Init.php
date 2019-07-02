ba61e8c9060bd06476ef50e46918f88bdfe7d514
Oyewole S. Abayomi <oyewoleabayomi@gmail.com>
2019-07-02 08:23:33 +0100

    init class

--- samsoft/Init.php
+++ samsoft/Init.php
@@ -2,8 +2,42 @@
 
     namespace Samsoft;
 
-    final class Init
+use Samsoft\Base\Enqueue;
+
+final class Init
     {
-        
+        /**
+         * Store all class inside array */   
+        public static function get_service(){
+            return [
+                //base class
+                Enqueue::class
+            ];
+        }
+
+        /**
+         * Initialize class and check if register method exists;
+         */
+        public static function register_services(){
+
+            foreach (self::get_service() as $class) {
+                # code...
+                $service = self::instantiate($class);
+
+                if( method_exists( $service, 'register' ) ){
+                    
+                    $service->register();
+
+                }
+
+            }
+        }
+
+        //Initialize class 
+        public static function instantiate( $class ){
+
+            return new $class();
+
+        }
     }
     
